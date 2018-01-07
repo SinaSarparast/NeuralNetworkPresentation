d99be42c9b72511d7b2843706abf12fc9b4d3601
werdnakof <andrewfok007@gmail.com>
2018-01-07 15:12:58 +0000

    added axes label

--- NN Demo in JS/js/test.js
+++ NN Demo in JS/js/test.js
@@ -70,13 +70,5 @@ function script() {
 			count++;
 		}
 	}
-
-
-	// test the network
-	// console.log(myNetwork.activate([0,0])); // [0.015020775950893527]
-	// console.log(myNetwork.activate([0,1])); // [0.9815816381088985]
-	// console.log(myNetwork.activate([1,0])); // [0.9871822457132193]
-	// console.log(myNetwork.activate([1,1])); // [0.012950087641929467]
-
 	return perf;
 }
