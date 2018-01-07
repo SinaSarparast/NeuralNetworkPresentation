a11f9260ac6edff7ce1ce6ea2d7466b96e4a64b4
werdnakof <andrewfok007@gmail.com>
2018-01-07 15:08:25 +0000

    updated with performance plots

--- NN Demo in JS/js/test.js
+++ NN Demo in JS/js/test.js
@@ -22,31 +22,61 @@ function script() {
 		output: outputLayer
 	});
 
+	var count = 0;
+	var perf = {
+		data: [],
+		labels: [],
+		raw: {a:[], b:[], c:[], d:[]}
+	};
+
 	// train the network
 	var learningRate = .3;
 	for (var i = 0; i < 20000; i++)
 	{
 		// 0,0 => 0
-		myNetwork.activate([0,0]);
+		var a = myNetwork.activate([0,0]);
 		myNetwork.propagate(learningRate, [0]);
 
 		// 0,1 => 1
-		myNetwork.activate([0,1]);
+		var b = myNetwork.activate([0,1]);
 		myNetwork.propagate(learningRate, [1]);
 
+
 		// 1,0 => 1
-		myNetwork.activate([1,0]);
+		var c = myNetwork.activate([1,0]);
 		myNetwork.propagate(learningRate, [1]);
 
 		// 1,1 => 0
-		myNetwork.activate([1,1]);
+		var d = myNetwork.activate([1,1]);
 		myNetwork.propagate(learningRate, [0]);
+
+
+		if(i % 100 == 0) {
+
+			var _a =  1.0 - a[0];
+
+			var _d = 1.0 - d[0];
+
+			var sum = _a + b[0] + c[0] + _d;
+
+			var avg = (sum / 4) * 100;
+
+			perf.data.push(avg);
+			perf.labels.push(count);
+			perf.raw.a.push(a[0]);
+			perf.raw.b.push(b[0]);
+			perf.raw.c.push(c[0]);
+			perf.raw.d.push(d[0]);
+			count++;
+		}
 	}
 
 
 	// test the network
-	console.log(myNetwork.activate([0,0])); // [0.015020775950893527]
-	console.log(myNetwork.activate([0,1])); // [0.9815816381088985]
-	console.log(myNetwork.activate([1,0])); // [0.9871822457132193]
-	console.log(myNetwork.activate([1,1])); // [0.012950087641929467]
+	// console.log(myNetwork.activate([0,0])); // [0.015020775950893527]
+	// console.log(myNetwork.activate([0,1])); // [0.9815816381088985]
+	// console.log(myNetwork.activate([1,0])); // [0.9871822457132193]
+	// console.log(myNetwork.activate([1,1])); // [0.012950087641929467]
+
+	return perf;
 }
